8561e4dc4179ecb155914fc0ca9efe845e6339b8
Tim Ellis <tirano2150@hotmail.com>
2020-07-13 11:47:56 +0100

    Update createJobs.groovy

--- vars/createJobs.groovy
+++ vars/createJobs.groovy
@@ -1,3 +1,20 @@
 def call() {
-  echo "Hello Tim"
+
+   node {
+
+    stage('Checkout') {
+      deleteDir()
+      sh """
+      git clone https://github.com/tranquilitybase-io/tb-gcp.git -b issue-398---Jenkins-CasC
+      """
+    }
+
+    stage("Create jobs") {
+      jobDsl targets: 'tb-jenkins/jobs/*.groovy',
+        removedJobAction: 'DELETE',
+        removedViewAction: 'DELETE',
+        lookupStrategy: 'SEED_JOB',
+        additionalClasspath: [].join('\n'),
+        additionalParameters: [repos: repos]
+    }
 }
